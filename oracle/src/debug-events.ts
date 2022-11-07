4bf9e73eda99fca022674e111c9d52d1e75bf97a
Gareth Davies <hello@garethtdavies.com>
2022-11-07 15:58:10 -0700

    Update debug-events.ts

--- oracle/src/debug-events.ts
+++ oracle/src/debug-events.ts
@@ -1,3 +1,7 @@
+// Helper script to decode public key to base58 encoded
+// When stored on chain a public key takes 2 fields (of 8 available)
+// e.g. 13117991216867267972578094433946611068215850158296483053051446309883779846074 and 0 (isOdd = false) 
+
 import {
   isReady,
   shutdown,
