2cc268214c7af3046287df9371a8a7e90d7f34d1
Gareth Davies <hello@garethtdavies.com>
2022-11-08 18:44:22 +0000

    Adding a simple price oracle example

---
+++ lambda-price/index.js
@@ -0,0 +1,45 @@
+"use strict";
+// This API returns the latest price data from Coingecko API
+// Can run locall with `npm run build && npm run locally` to simulate the lambda function
+Object.defineProperty(exports, "__esModule", { value: true });
+const snarkyjs_1 = require("snarkyjs");
+exports.handler = async (event) => {
+    await snarkyjs_1.isReady;
+    // TODO REPLACE THIS WITH OUR OWN KEY SERVER BY SECRET ENV
+    // Using this as same as examples
+    const privateKey = snarkyjs_1.PrivateKey.fromBase58("EKF65JKw9Q1XWLDZyZNGysBbYG21QbJf3a4xnEoZPZ28LKYGMw53");
+    // We compute the public key associated with our private key
+    const signingKey = privateKey.toPublicKey();
+    // Make the API call
+    const priceData = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=mina-protocol&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true").then((response) => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Something went wrong fetching the data.');
+    }).catch((error) => {
+        console.log(error);
+    });
+    console.log(priceData);
+    // This price is in dollars e.g. 0.635231 - we want in cents and then rounded to the nearest cent
+    const price = snarkyjs_1.UInt32.fromNumber(Math.round(priceData["mina-protocol"].usd));
+    const marketCap = snarkyjs_1.UInt64.fromNumber(Math.round(priceData["mina-protocol"].usd_market_cap));
+    const lastUpdate = snarkyjs_1.UInt64.fromNumber(priceData["mina-protocol"].last_updated_at);
+    // Sign all the data
+    const signedData = price.toFields().concat(marketCap.toFields()).concat(lastUpdate.toFields());
+    const signature = snarkyjs_1.Signature.create(privateKey, signedData);
+    const data = {
+        data: {
+            "priceCents": price,
+            "lastUpdate": lastUpdate,
+            "marketCap": marketCap,
+        },
+        signature: signature,
+        publicKey: signingKey,
+    };
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(data),
+    };
+    console.log(response);
+    return response;
+};
---
+++ lambda-price/index.ts
@@ -0,0 +1,70 @@
+// This API returns the latest price data from Coingecko API
+// Can run locall with `npm run build && npm run locally` to simulate the lambda function
+
+import { format } from "path";
+import { isReady, PublicKey, PrivateKey, Field, Signature, UInt32, UInt64 } from "snarkyjs";
+
+exports.handler = async (event) => {
+
+  await isReady;
+
+  type Data = {
+    data: {
+      "priceCents": UInt32,
+      "lastUpdate": UInt64,
+      "marketCap": UInt64,
+    },
+    signature: Signature,
+    publicKey: PublicKey
+  };
+
+  // TODO REPLACE THIS WITH OUR OWN KEY SERVER BY SECRET ENV
+  // Using this as same as examples
+  const privateKey = PrivateKey.fromBase58(
+    "EKF65JKw9Q1XWLDZyZNGysBbYG21QbJf3a4xnEoZPZ28LKYGMw53"
+  );
+  // We compute the public key associated with our private key
+  const signingKey = privateKey.toPublicKey();
+
+  // Make the API call
+  const priceData = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=mina-protocol&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true").then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Something went wrong fetching the data.');
+  }).catch((error: any) => {
+    console.log(error)
+  });
+
+  console.log(priceData);
+
+  // This price is in dollars e.g. 0.635231 - we want in cents and then rounded to the nearest cent
+  const price = UInt32.fromNumber(Math.round(priceData["mina-protocol"].usd));
+  const marketCap = UInt64.fromNumber(Math.round(priceData["mina-protocol"].usd_market_cap));
+  const lastUpdate = UInt64.fromNumber(priceData["mina-protocol"].last_updated_at);
+
+  // Sign all the data
+  const signedData = price.toFields().concat(marketCap.toFields()).concat(lastUpdate.toFields());
+
+  const signature = Signature.create(privateKey, signedData);
+
+  const data: Data = {
+    data: {
+      "priceCents": price,
+      "lastUpdate": lastUpdate,
+      "marketCap": marketCap,
+    },
+    signature: signature,
+    publicKey: signingKey,
+  };
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+
+  console.log(response);
+
+  return response;
+
+};
