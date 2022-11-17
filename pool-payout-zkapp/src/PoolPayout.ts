fd6790969909af2ac37f3b4c7cef4fcab4b62c43
Gareth Davies <hello@garethtdavies.com>
2022-11-17 21:22:29 +0000

    More work on zkApp

--- pool-payout-zkapp/src/PoolPayout.ts
+++ pool-payout-zkapp/src/PoolPayout.ts
@@ -1,3 +1,10 @@
+/*
+* First we get the data from the oracle and verify it by checking the signature
+* Then we get the latest index from the chain
+* Then we batch up 9 accounts
+* We do all the assertions, right epoch, right index etc...
+*/
+
 import {
   Field,
   SmartContract,
@@ -10,14 +17,127 @@ import {
   Signature,
   UInt64,
   UInt32,
+  verify,
+  Struct,
+  Circuit,
 } from 'snarkyjs';
 
 // The public key of our trusted data provider
 const ORACLE_PUBLIC_KEY =
   'B62qphyUJg3TjMKi74T2rF8Yer5rQjBr1UyEG7Wg9XEYAHjaSiSqFv1';
 
+// Using this value as a test as a nice number of delegates
+const VALIDATOR_PUBLIC_KEY = 'B62qjhiEXP45KEk8Fch4FnYJQ7UMMfiR3hq9ZeMUZ8ia3MbfEteSYDg';
+
+// This matches our output
+export class Reward extends Struct({
+  index: UInt32,
+  publicKey: PublicKey,
+  delegatingBalance: UInt64,
+  rewards: UInt64
+}) { // Have the data concatenated here?
+}
+
+export class Rewards2 extends Struct(
+  [Reward, Reward]) { }
+
 export class PoolPayout extends SmartContract {
 
   // What state variables do we need to store
+
+  // The Oracle public key (takes 2)
   @state(PublicKey) oraclePublicKey = State<PublicKey>();
+
+  // The latest epoch - will init this variable to have a starting point
+  @state(Field) currentEpoch = State<Field>();
+
+  // The current index of the payout run
+  @state(Field) currentIndex = State<Field>();
+
+  // Validator key
+  @state(PublicKey) validatorPublicKey = State<PublicKey>();
+
+  // Deploy this - tighten permissions and move init stuff to an init method
+  deploy(args: DeployArgs) {
+    super.deploy(args);
+    this.setPermissions({
+      ...Permissions.default(),
+      editState: Permissions.proofOrSignature(),
+      send: Permissions.proofOrSignature(),
+      setDelegate: Permissions.impossible(),
+      setZkappUri: Permissions.impossible(),
+    });
+
+    // Move this to an init() method
+    this.oraclePublicKey.set(PublicKey.fromBase58(ORACLE_PUBLIC_KEY));
+    this.currentEpoch.set(Field(39));
+    this.currentIndex.set(Field(0));
+    this.validatorPublicKey.set(PublicKey.fromBase58(VALIDATOR_PUBLIC_KEY));
+
+    // TODO emit an event for each payout?
+  }
+
+  /*
+  @method sendReward(accounts: Rewards2) {
+
+    // Assert the validating key on chain
+    let oraclePublicKey = this.oraclePublicKey.get();
+    this.oraclePublicKey.assertEquals(oraclePublicKey);
+
+    // Assert the epoch
+    let currentEpoch = this.currentEpoch.get();
+    //currentEpoch.assertEquals(Field(39));
+    Circuit.log(currentEpoch);
+
+    let currentIndex = this.currentIndex.get();
+    Circuit.log(currentIndex);
+
+    for (let [_, value] of Object.entries(accounts)) {
+      this.send({
+        to: value.publicKey,
+        //amount: value.rewards,
+        amount: 1_000_000
+      });
+    };
+    // TODO lets send the correct amount when claimed
+  }
+  */
+
+  @method sendReward(account: Reward) {
+
+    // Assert the validating key on chain
+    let oraclePublicKey = this.oraclePublicKey.get();
+    this.oraclePublicKey.assertEquals(oraclePublicKey);
+
+    // Assert the epoch
+    let currentEpoch = this.currentEpoch.get();
+    this.currentEpoch.assertEquals(this.currentEpoch.get());
+    Circuit.log(currentEpoch);
+
+    let currentIndex = this.currentIndex.get();
+    this.currentIndex.assertEquals(this.currentIndex.get());
+    Circuit.log(currentIndex);
+
+    this.send({
+      to: account.publicKey,
+      //amount: account.rewards,
+      amount: 1_000_000
+    });
+
+  }
+
+  @method claimReward() {
+    // TO IMPLEMENT
+    // Can only claim rewards when all payouts have been sent for an epoch
+    // This also updates the epoch to the next epoch, i.e. epoch is over...
+  }
+
+  @method closeEpoch() {
+    // maybe ensure everything claimed
+  }
+
 }
+
+// We'll use a seperate fee payer so we can increment the nonces properly. This also simplifies the handling of fees.
+
+
--- pool-payout-zkapp/src/main.ts
+++ pool-payout-zkapp/src/main.ts
@@ -1,4 +1,4 @@
-import { PoolPayout } from './PoolPayout.js';
+import { PoolPayout, Reward, Rewards2 } from './PoolPayout.js';
 
 import {
   isReady,
@@ -24,6 +24,72 @@ import {
   Mina.setActiveInstance(Berkeley);
 
   let transactionFee = 100_000_000;
+  let feePayerPrivateKey = PrivateKey.fromBase58(
+    'EKDvE7umHorQrXFq1AAwV4zEDLGtZuqpn1mhsgxvYRneUpKxRUF8'
+  );
+
+  const zkAppAddress = PublicKey.fromBase58("B62qmFzf13S4WpbNnUZAYnwwCPEgaRiLZF7ZxGKE7LHKbZXMYe3LEYM");
+  const zkAppInstance = new PoolPayout(zkAppAddress);
+
+  console.log('Compiling smart contract...');
+  await PoolPayout.compile();
+
+  // Function URL
+  let functionUrl = "https://kodem6bg3gatbplrmoiy2sxnty0wfrhp.lambda-url.us-west-2.on.aws/?publicKey=B62qjhiEXP45KEk8Fch4FnYJQ7UMMfiR3hq9ZeMUZ8ia3MbfEteSYDg&epoch=39"
+
+  console.log(functionUrl);
+
+  // Make the API call
+  const data = await fetch(functionUrl).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Something went wrong fetching the data.');
+  }).catch((error: any) => {
+    console.log(error)
+  });
+
+  // Take a small slice to test batch sending payouts
+  let testData = data.rewards.slice(0, 2);
+
+  // This always need to be a fixed size so we would have to create dummy rewards to fill it
+
+  console.log(testData);
+
+  let rewardFields: Rewards2 = [];
+
+  // Now we have to convert this to Fields
+  testData.forEach((element) => {
+    rewardFields.push({
+      index: UInt32.from(element.index),
+      publicKey: PublicKey.fromBase58(element.publicKey),
+      delegatingBalance: UInt64.from(element.delegatingBalance),
+      rewards: UInt64.from(element.rewards)
+    });
+  });
+
+  console.log(rewardFields.length);
+
+  try {
+    let transaction = await Mina.transaction(
+      { feePayerKey: feePayerPrivateKey, fee: transactionFee },
+      () => {
+        rewardFields.forEach(function (value) {
+          zkAppInstance.sendReward(value);
+        });
+      }
+    );
+
+    console.log("Proving transaction");
+    await transaction.prove();
+
+    console.log("Sending transaction");
+    console.log(transaction.toPretty());
+    await transaction.send();
+  } catch (error: any) {
+    console.log("There was an issue");
+    console.log(error.message);
+  }
 
   // ----------------------------------------------------
   console.log('Shutting down');
